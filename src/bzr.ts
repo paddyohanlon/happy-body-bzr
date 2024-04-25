b9575a61550af06877e5e2d332da0ab98ea1df86
Paddy <ohanlon.paddy@gmail.com>
2024-04-25 14:04:38 +0200

    Add env vars for Bzr config

--- src/bzr.ts
+++ src/bzr.ts
@@ -2,8 +2,8 @@ import { BazaarApp, type BazaarOptions } from "@bzr/bazaar";
 import type { AppUser } from "./types";
 
 const options: BazaarOptions = {
-  appId: "test",
-  loginRedirectUri: "http://localhost:5173",
+  appId: import.meta.env.VITE_BAZAAR_APP_ID,
+  loginRedirectUri: import.meta.env.VITE_BAZAAR_REDIRECT_URI,
   onApiConnectError: async (bzr) => bzr.logOut(),
 };
 
